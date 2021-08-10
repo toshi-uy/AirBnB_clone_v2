72e85ab1bfa1523169413201dc5f2fb5508fa273
toshi-uy <s_borgia@hotmail.com>
2021-08-10 10:50:01 -0300

    added -*

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,9 +6,9 @@
 -- hbnb_dev should have SELECT privilege on the database performance_schema (and only this database)
 -- If the database hbnb_dev_db or the user hbnb_dev already exists, your script should not fail
 
+CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
 SET PASSWORD FOR  'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
-CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost'
-GRANT ALL ON `hbnb_dev_db` TO 'hbnb_dev'@'localhost';
+GRANT ALL ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';

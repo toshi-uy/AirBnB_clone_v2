95d0770708886116e7e63f9dd15d1e5c8af0d62f
toshi-uy <s_borgia@hotmail.com>
2021-08-10 10:48:59 -0300

    added grant usage

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -9,5 +9,6 @@
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
 SET PASSWORD FOR  'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
 CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost'
 GRANT ALL ON `hbnb_dev_db` TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';

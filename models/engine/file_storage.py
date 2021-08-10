43e9717cff7c5b560836d9c914f356a13e8d465f
toshi-uy <s_borgia@hotmail.com>
2021-08-10 11:17:10 -0300

    testing delete

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -48,3 +48,10 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """delete obj from __objects if it’s inside
+        if obj is equal to None, the method should not do anything"""
+        if obj:
+            if obj in self.__objects:
+                del(self.__objects[obj])

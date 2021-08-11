dee9f836183d452190a61746ff9ca1138255a5e6
RaimundoGallino <raigallino0@gmail.com>
2021-08-11 14:06:45 -0300

    tas-6-correction-requirement

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from os import getenv
 
 class City(BaseModel, Base):
@@ -10,7 +11,7 @@ class City(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship("Place", backref="cities", cascade="all, delete")
+        places = relationship("Place", backref="cities", cascade="all, delete-orphan")
     else:
         name = ""
         state_id = ""
--- models/state.py
+++ models/state.py
@@ -12,7 +12,7 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="all, delete")
+        cities = relationship("City", backref="state", cascade="all, delete-orphan")
     else:
         name = ""
         @property

66c4f7c20540793ab234e9939bb42b5acd1affa4
RaimundoGallino <raigallino0@gmail.com>
2021-08-11 14:37:40 -0300

    tas-8-correction

--- models/city.py
+++ models/city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.places import Place
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from os import getenv
--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
+from models.places import Place
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 

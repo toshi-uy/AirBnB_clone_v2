5dd435ddac146ae02347477a8c85692c6837ae30
RaimundoGallino <raigallino0@gmail.com>
2021-08-11 20:10:47 -0300

    task-10-first check

--- models/base_model.py
+++ models/base_model.py
@@ -16,10 +16,12 @@ class BaseModel():
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        if kwargs:
+        if not kwargs:
+            from models import storage
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
             for key, value in kwargs.items():
                 if key == "__class__":
                     continue
@@ -28,6 +30,8 @@ class BaseModel():
                     setattr(self, key, pichu)
                 else:
                     setattr(self, key, value)
+            if 'id' not in kwargs:
+                setattr(self, 'id', str(uuid.uuid4()))
 
     def __str__(self):
         """Returns a string representation of the instance"""

455bf771bcfb3c16fd49f6f0cdfd47f977648ca2
toshi-uy <s_borgia@hotmail.com>
2021-08-26 20:37:47 -0300

    changed deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -29,15 +29,14 @@ def do_deploy(archive_path):
     if path.exists(archive_path):
         try:
             filename = archive_path[9:-4]
-            fullname = "/data/web_static/releases/{}/".format(filename)
             put(archive_path, "/tmp/")
-            run("mkdir -p {}".format(fullname))
-            run("tar -xzf /tmp/{} -C ".format(filename + ".tgz") + fullname)
+            run("mkdir -p /data/web_static/releases/{}/".format(filename))
+            run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(filename + ".tgz", filename))
             run("rm /tmp/{}".format(filename + ".tgz"))
-            run("mv {}/web_static/*".format(fullname) + fullname)
-            run("rm -rf {}/web_static".format(fullname))
+            run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(filename, filename))
+            run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
             run("rm -rf /data/web_static/current")
-            run("ln -s {} /data/web_static/current".format(fullname))
+            run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(filename))
             print("New version deployed!")
             return True
         except:

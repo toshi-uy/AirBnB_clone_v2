5fb17494c1464d1c4ac34704ec64986e404a7cf3
toshi-uy <s_borgia@hotmail.com>
2021-08-26 18:16:28 -0300

    one blankline

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -35,8 +35,8 @@ def do_deploy(archive_path):
         run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
             .format(filename + ".tgz", filename))
         run("rm /tmp/{}".format(filename + ".tgz"))
-        run("mv /data/web_static/releases/{}/web_static/*\
-             /data/web_static/releases/{}}/".format(filename, filename))
+        run("mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}}/".format(filename, filename))
         run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
         run("rm -rf /data/web_static/current")
         run("ln -sf /data/web_static/releases/{}/ /data/web_static/current"

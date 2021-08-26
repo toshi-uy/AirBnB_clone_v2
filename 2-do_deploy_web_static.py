706b80245d02cdef345a440b01b1ee917f16934d
toshi-uy <s_borgia@hotmail.com>
2021-08-26 18:58:01 -0300

    fixing stuff, added print

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -26,21 +26,20 @@ def do_deploy(archive_path):
     """distributes an archive to your web servers, using the
     function do_deploy"""
 
-    if not archive_path:
-        return False
-    try:
-        filename = archive_path[9:-4]
-        put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/{}/".format(filename))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(filename + ".tgz", filename))
-        run("rm /tmp/{}".format(filename + ".tgz"))
-        run("mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}}/".format(filename, filename))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
-        run("rm -rf /data/web_static/current")
-        run("ln -sf /data/web_static/releases/{}/ /data/web_static/current"
-            .format(filename))
-        return True
-    except:
+    if path.exists(archive_path):
+        try:
+            filename = archive_path[9:-4]
+            put(archive_path, "/tmp/")
+            run("mkdir -p /data/web_static/releases/{}/".format(filename))
+            run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(filename + ".tgz", filename))
+            run("rm /tmp/{}".format(filename + ".tgz"))
+            run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(filename, filename))
+            run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
+            run("rm -rf /data/web_static/current")
+            run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(filename))
+            print("New version deployed!")
+            return True
+        except:
+            return False
+    else:
         return False

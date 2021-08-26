25282477f8d4c1847e249fae1e8328eb714d2c4e
toshi-uy <s_borgia@hotmail.com>
2021-08-26 20:53:50 -0300

    testing

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -31,12 +31,17 @@ def do_deploy(archive_path):
             filename = archive_path[9:-4]
             put(archive_path, "/tmp/")
             run("mkdir -p /data/web_static/releases/{}/".format(filename))
-            run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(filename + ".tgz", filename))
+            run("tar -xzf /tmp/{}".format(filename + ".tgz") +
+                " -C /data/web_static/releases/{}/".format(filename))
             run("rm /tmp/{}".format(filename + ".tgz"))
-            run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(filename, filename))
-            run("rm -rf /data/web_static/releases/{}/web_static".format(filename))
+            run("mv /data/web_static/releases/" +
+                "{}/web_static/*".format(filename)
+                + " /data/web_static/releases/{}/".format(filename))
+            run("rm -rf /data/web_static/releases/" +
+                "{}/web_static".format(filename))
             run("rm -rf /data/web_static/current")
-            run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(filename))
+            run("ln -s /data/web_static/releases/{}/" +
+                " /data/web_static/current".format(filename))
             print("New version deployed!")
             return True
         except:

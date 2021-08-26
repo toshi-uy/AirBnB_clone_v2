e95b9000293c8a8b291fade53de66f9308c932eb
toshi-uy <s_borgia@hotmail.com>
2021-08-26 20:12:36 -0300

    stored in answ

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -31,14 +31,14 @@ def do_deploy(archive_path):
             filename = archive_path[9:-4]
             put(archive_path, "/tmp/")
             run("mkdir -p /data/web_static/releases/{}/".format(filename))
-            run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-                .format(filename + ".tgz", filename))
+            run("tar -xzf /tmp/{}".format(filename + ".tgz") +
+                " -C /data/web_static/releases/{}/".format(filename))
             run("rm /tmp/{}".format(filename + ".tgz"))
-            run("mv /data/web_static/releases/{}/web_static/*"
-                .format(filename) +
-                " /data/web_static/releases/{}/".format(filename))
-            run("rm -rf /data/web_static/releases/{}/web_static"
-                .format(filename))
+            run("mv /data/web_static/releases/" +
+                "{}/web_static/*".format(filename)
+                + " /data/web_static/releases/{}/".format(filename))
+            run("rm -rf /data/web_static/releases/" +
+                "{}/web_static".format(filename))
             run("rm -rf /data/web_static/current")
             run("ln -s /data/web_static/releases/{}/" +
                 " /data/web_static/current".format(filename))

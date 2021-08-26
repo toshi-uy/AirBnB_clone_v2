659aa64738a57c8af0fa4c77a784d79ace67cb4d
toshi-uy <s_borgia@hotmail.com>
2021-08-26 15:24:04 -0300

    testing api and cvzf flags

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz archive from the contents of the
+web_static folder of your AirBnB Clone repo, using the function do_pack
+"""
+from fabric.api import local
+import time
+from os import path
+
+def do_pack():
+    """generates a .tgz archive from the contents of the web_static folder"""
+    today = time.strftime("%Y%m%d%H%M%S", time.gmtime())
+    filename = "versions/web_static_" + today + ".tgz"
+    try:
+        local("tar -cvzf " + filename + "web_static")
+        return filename
+    except:
+        return None

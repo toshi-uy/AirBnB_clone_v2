f0458aa6c264cc0d043eb4936cf45d191d09bd7c
toshi-uy <s_borgia@hotmail.com>
2021-08-26 14:13:02 -0300

    testig static content

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Bash script that sets up your web servers for the deployment of web_static
+apt-get upodate -y
+apt-get install -y nginx
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+touch /data/web_static/releases/test/index.html
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -hR ubuntu:ubuntu /data/
+
+printf %s "server {
+    location /hbnb_static/ {
+        alias /data/web_static/current/;
+        autoindex off;
+    }
+}" > /etc/nginx/sites-available/default                           
+service nginx restart

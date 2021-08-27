1f3d0b39f40da79535180af6ca3b4152489b1723
toshi-uy <s_borgia@hotmail.com>
2021-08-26 21:38:12 -0300

    added puppet

---
+++ 101-setup_web_static.pp
@@ -0,0 +1,42 @@
+# Puppet for setup - Redo the task #0 but by using Puppet:
+exec {'Nginx':
+     command => 'sudo apt-get update; sudo apt-get install nginx -y',
+     provider => shell
+}
+
+exec {'folders':
+     command => 'sudo mkdir -p /data/web_static/releases/test/;
+     	     	 sudo mkdir -p /data/web_static/shared/',
+     provider => shell,
+     require => Exec['Nginx']
+}
+
+exec {'index':
+     command => 'echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html',
+     provider => shell,
+     require => Exec['folders']
+}
+
+exec {'Soft Link':
+     command => 'ln -sf /data/web_static/releases/test/ /data/web_static/current',
+     provider => shell,
+     require => Exec['index']
+}
+
+exec {'chown':
+     command => 'chown -R ubuntu:ubuntu /data/',
+     provider => shell,
+     require => Exec['Soft Link']
+}
+
+exec {'Location':
+     command => 'echo -e "server {\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n}" > /etc/nginx/sites-available/default',
+     provider => shell,
+     require => Exec['chown']
+}
+
+exec {'Restart':
+     command => 'service nginx restart',
+     provider => shell,
+     require => Exec['Location']
+}

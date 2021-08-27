9c9f174183f12c1adfd524b6947d2fbdff4fbbca
toshi-uy <s_borgia@hotmail.com>
2021-08-26 21:23:21 -0300

    added sudo

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -32,7 +32,7 @@ def do_deploy(archive_path):
             file_name = archive_path.split("/")[1]
             file_name2 = file_name.split(".")[0]
             final_name = "/data/web_static/releases/" + file_name2 + "/"
-            run("sudo mkdir -p " + final_name)
+            run("mkdir -p " + final_name)
             run("tar -xzf /tmp/" + file_name + " -C " + final_name)
             run("rm /tmp/" + file_name)
             run("mv " + final_name + "web_static/* " + final_name)

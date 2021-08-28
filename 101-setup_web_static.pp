444b39db91438a5e8af7223289e7df526637a868
toshi-uy <s_borgia@hotmail.com>
2021-08-28 16:30:16 -0300

    back to echo

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -30,7 +30,7 @@ exec {'chown':
 }
 
 exec {'Location':
-     command => 'sed -i "\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default',
+     command => 'echo -e "\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" > /etc/nginx/sites-available/default',
      provider => shell,
      require => Exec['chown']
 }

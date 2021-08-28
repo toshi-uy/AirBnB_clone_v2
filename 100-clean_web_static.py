f906844e5c8b49df3aa8161a603792490469dcf8
toshi-uy <s_borgia@hotmail.com>
2021-08-28 19:51:21 -0300

    changed if it's bigger than 2

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -65,5 +65,5 @@ def do_clean(number=0):
        run('cd /data/web_static/releases; ls | sort -n | tail -n +2 | xargs rm -rf && echo "releases cleaned"')
     else:
         number += 1
-        local('cd versions; ls | sort -n | tail -n {} | xargs rm -rf && echo "versions cleaned"'.format(number))
-        run('cd /data/web_static/releases; ls | sort -n | tail -n +3 | xargs rm -rf && echo "releases cleaned"'.format(number))
+        local('cd versions; ls | sort -n | tail -n +{} | xargs rm -rf && echo "versions cleaned"'.format(number))
+        run('cd /data/web_static/releases; ls | sort -n | tail -n +{} | xargs rm -rf && echo "releases cleaned"'.format(number))

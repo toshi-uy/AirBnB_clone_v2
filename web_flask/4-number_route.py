1ba3f58c14e160ee9d8c4490c5258969760aefa7
toshi-uy <s_borgia@hotmail.com>
2021-09-10 20:14:06 -0300

    added number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application:
+-Your web application must be listening on 0.0.0.0, port 5000
+-Routes:
+    /: display “Hello HBNB!”
+You must use the option strict_slashes=False in your route definition
+"""
+from flask import Flask, escape
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_world():
+    """ Default method """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """ Default method """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    """ Default method """
+    new = text.replace('_', ' ')
+    return 'C %s' % escape(new)
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python(text="is cool"):
+    """ Default method """
+    new = text.replace('_', ' ')
+    return 'Python %s' % escape(new)
+
+
+@app.route('/number/<n>')
+def python(n):
+    """ Default method """
+    if type(n) is int:
+        return '%d is a number' % escape(n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

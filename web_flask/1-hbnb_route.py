bf010c93fc5d5feef780c7c906953ec2f71dcf7e
toshi-uy <s_borgia@hotmail.com>
2021-09-10 19:17:02 -0300

    added file 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application:
+-Your web application must be listening on 0.0.0.0, port 5000
+-Routes:
+    /: display “Hello HBNB!”
+You must use the option strict_slashes=False in your route definition
+"""
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_world():
+    """ Default method """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello_world():
+    """ Default method """
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

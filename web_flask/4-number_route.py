dff0f99e25a33cbe8f5c5036e71c9cb8dbbbdd18
toshi-uy <s_borgia@hotmail.com>
2021-09-10 20:24:08 -0300

    trying

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -42,7 +42,8 @@ def python(text="is cool"):
 @app.route('/number/<n>')
 def number(n):
     """ Default method """
-    return '{:d} is a number'.format(n)
+    if int(n) is int:
+        return '{} is a number'.format(n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

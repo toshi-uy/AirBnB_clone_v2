c2132a76df1329ff2e54df4c747395f61ac217e6
toshi-uy <s_borgia@hotmail.com>
2021-09-10 20:17:19 -0300

    removed escape

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -42,7 +42,7 @@ def python(text="is cool"):
 def number(n):
     """ Default method """
     if type(n) is int:
-        return '%d is a number' % escape(n)
+        return '%d is a number' % n
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

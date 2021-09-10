2f46bdb5344060c4e66606d155d215cc3b4842bc
toshi-uy <s_borgia@hotmail.com>
2021-09-10 18:18:52 -0300

    changed if

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,15 +7,14 @@ Script that starts a Flask web application:
 You must use the option strict_slashes=False in your route definition
 """
 from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
 
+
+@app.route('/')
+def hello_world():
+    """ Default method """
+    return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app = Flask(__name__)
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
-
-
-    @app.route('/')
-    def hello_world():
-        """ Default method """
-        return 'Hello HBNB!'

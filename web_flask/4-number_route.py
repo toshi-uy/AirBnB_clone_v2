c64f7b7961d17601cb8157daa25ccfcfe45f1af5
toshi-uy <s_borgia@hotmail.com>
2021-09-10 20:51:54 -0300

    try, execpt

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -45,7 +45,7 @@ def number(n):
     try:
         return '{:d} is a number'.format(int(n))
     except:
-        pass
+        return n
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

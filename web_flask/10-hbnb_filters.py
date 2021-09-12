e9e6ed3af03a9ec8e5329c6b295d1533f445ba72
toshi-uy <s_borgia@hotmail.com>
2021-09-12 15:08:33 -0300

    added asked files and changed 10 filters site

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/hbnb_filters')
+def hbnb_filters():
+    """ Default method """
+    states = storage.all(State)
+    amenities = storage.all('Amenity')
+    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
+
+
+@app.teardown_appcontext
+def close(self):
+    """function that remove the current SQLAlchemy Session"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

7073e40f467b0911edc9a176a9372b0810e5f889
toshi-uy <s_borgia@hotmail.com>
2021-09-12 12:55:06 -0300

    testing flag in jinja

---
+++ web_flask/9-states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/states')
+@app.route('/states/<id>')
+def states_id(id=None):
+    """ Default method """
+    states = storage.all(State)
+    if (id):
+        return render_template('9-states.html.html', states=states, id=id)
+    return render_template('9-states.html.html', states=states)
+
+
+@app.teardown_appcontext
+def close(self):
+    """function that remove the current SQLAlchemy Session"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

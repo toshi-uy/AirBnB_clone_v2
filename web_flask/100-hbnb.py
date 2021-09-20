c341b578236f3ac6715db3fb8630ddf1f0eea54b
toshi-uy <s_borgia@hotmail.com>
2021-09-20 11:05:26 -0300

    added 100 places

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -6,17 +6,19 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.amenity import Amenity
+from models.place import Place
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route('/hbnb_filters')
-def hbnb_filters():
+@app.route('/hbnb')
+def hbnb_places():
     """ Default method """
     states = storage.all(State)
     amenities = storage.all(Amenity)
-    return render_template('10-hbnb_filters.html',
-                           states=states, amenities=amenities)
+    places = storage.all(Place)
+    return render_template('100-hbnb.html', states=states,
+                           amenities=amenities, places=places)
 
 
 @app.teardown_appcontext
